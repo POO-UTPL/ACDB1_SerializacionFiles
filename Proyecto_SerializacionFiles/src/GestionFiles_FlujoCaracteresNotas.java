2334d70cf32ec13205c801a4dee2fcb560026620
pdirene <macbookpro@MacBookPro-pdirene.local>
2025-05-30 15:54:38 -0500

    act

--- Proyecto_SerializacionFiles/src/GestionFiles_FlujoCaracteresNotas.java
+++ Proyecto_SerializacionFiles/src/GestionFiles_FlujoCaracteresNotas.java
@@ -1,7 +1,10 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.text.NumberFormat;
+import java.text.ParseException;
 import java.util.Arrays;
 import java.util.Formatter;
+import java.util.Locale;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.logging.Level;
@@ -43,6 +46,7 @@ public class GestionFiles_FlujoCaracteresNotas {
     public static void lecturaNotasFiles(String notasLeidas[][]){
         try {
             Scanner objLecturaFile = new Scanner(new File("notasEntrada.csv"));
+            objLecturaFile.useLocale(Locale.US);
             while(objLecturaFile.hasNextLine()){
                 String materiaNotas[] = objLecturaFile.nextLine().split(";");
                 System.out.println(Arrays.toString(materiaNotas));
@@ -53,12 +57,13 @@ public class GestionFiles_FlujoCaracteresNotas {
         }
     }
     public static void calcularResultados(String resultados[][], String notasXavierEntrada[][]){
+        //double nota1B1 = Double.parseDouble(notasXavierEntrada[0][1]);
         for (int i = 0; i < notasXavierEntrada.length; i++) {
-            double promedio = (  Double.parseDouble(notasXavierEntrada[i][1]) 
-                               + Double.parseDouble(notasXavierEntrada[i][2]) 
-                                                ) / 2;
-            //resultados[i][0] = String.valueOf(promedio);
-            //resultados[i][1] = (promedio >= 6.5 ) ? "APRO" : "REPRO" ;
+            System.out.println(notasXavierEntrada[0][1]);
+                //double nota1B1 = Double.parseDouble(notasXavierEntrada[i][1]);
+                //System.out.println(nota1B1);
+                //double nota2B1 = fn.parse(notasXavierEntrada[i][2]).doubleValue();
+                //resultados[i][1] = ((nota1B1=nota2B1)/2 >= 6.5 ) ? "APRO" : "REPRO" ;
         }
     }
 }
