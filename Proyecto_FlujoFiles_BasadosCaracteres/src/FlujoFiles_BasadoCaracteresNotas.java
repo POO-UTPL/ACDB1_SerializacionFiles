cfd40af8657f030b2ab06964b2f270e83f4ae9c2
pdirene <macbookpro@MacBookPro-pdirene.local>
2025-05-30 17:46:29 -0500

    act

--- Proyecto_FlujoFiles_BasadosCaracteres/src/FlujoFiles_BasadoCaracteresNotas.java
+++ Proyecto_FlujoFiles_BasadosCaracteres/src/FlujoFiles_BasadoCaracteresNotas.java
@@ -9,13 +9,14 @@ public class FlujoFiles_BasadoCaracteresNotas {
         Random ale = new Random();
         try {
             Formatter outNotas = new Formatter(new File("NotasEntrada.csv"));
+            outNotas.format("%s;%s;%s\n", "NOMB_MATERIA", "NOTA 1 BIM1", "NOTA 2 BIM1");
             for (int i = 0; i < 10; i++) {
                 //random.nextInt(max - min + 1) + min;
                 int nota1B1 = ale.nextInt(10 - 0 + 1) + 0;
                 int nota2B1 = ale.nextInt(10 - 0 + 1) + 0;
-                outNotas.format("Materia_%d;%d;%d", (i+1), nota1B1, nota2B1);
-                outNotas.close();
+                outNotas.format("Materia_%d;%d;%d\n", (i+1), nota1B1, nota2B1);
             }
+            outNotas.close();
         } catch (FileNotFoundException ex) {
             Logger.getLogger(FlujoFiles_BasadoCaracteresNotas.class.getName()).log(Level.SEVERE, null, ex);
         }
