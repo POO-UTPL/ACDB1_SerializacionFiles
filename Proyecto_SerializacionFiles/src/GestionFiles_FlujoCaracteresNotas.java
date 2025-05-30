9ddc7b306e6f4c3bf475a939b979d62743412d03
pdireneutpl <pdirene@utpl.edu.ec>
2025-05-30 12:11:06 -0500

    act

--- Proyecto_SerializacionFiles/src/GestionFiles_FlujoCaracteresNotas.java
+++ Proyecto_SerializacionFiles/src/GestionFiles_FlujoCaracteresNotas.java
@@ -1,8 +1,14 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Formatter;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 public class GestionFiles_FlujoCaracteresNotas {
     public static void main(String[] args) {
         String notasXavier[][] = new String[6][3];
         generarNotas(notasXavier);
+        persistirNotasFiles(notasXavier);
     }
     
     public static void generarNotas(String notas[][]){
@@ -15,6 +21,17 @@ public class GestionFiles_FlujoCaracteresNotas {
             notas[i][0] = nombreMateria;
             notas[i][1] = String.valueOf(notaB1);
             notas[i][2] = String.valueOf(notaB2);
+        }
+    }
+    public static void persistirNotasFiles(String notas[][]){
+        try {
+            Formatter objPersistenciaFile = new Formatter(new File("notasEntrada.csv"));
+            for (int i = 0; i < notas.length; i++) {
+                objPersistenciaFile.format("%s;%s;%s;", notas[i][0], notas[i][1], notas[i][2]);
+            }
+            objPersistenciaFile.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(GestionFiles_FlujoCaracteresNotas.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
