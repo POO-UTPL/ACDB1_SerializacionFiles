a9a2e289f0cf2fd2111210cac033de5f1cc94180
pdirene <macbookpro@MacBookPro-pdirene.local>
2025-05-30 16:57:44 -0500

    act

--- Proyecto_FlujoFiles_BasadosCaracteres/src/FlujoFiles_BasadosCaracteres.java
+++ Proyecto_FlujoFiles_BasadosCaracteres/src/FlujoFiles_BasadosCaracteres.java
@@ -2,23 +2,42 @@ import java.util.Scanner;
 import java.util.Formatter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 public class FlujoFiles_BasadosCaracteres {
-    public static void main(String[] args) {
-        /*//FLUJO SALIDA: del .java a la cmd
+    public static void main(String[] args)  {
+        /* //FLUJO SALIDA: del .java a la cmd
         System.out.println("Hola mundo");
         //FLUJO ENTRADA: del cmd al .java
         Scanner teclado = new Scanner(System.in); 
-        System.out.println(teclado.nextLine());  */
+        System.out.println(teclado.nextLine());
+        int num1 = teclado.nextInt(); */
         
-        //FLUJO SALIDA: del .java al file
+        //FLUJO SALIDA = Escritura: del .java al file
         try { //Gestion de errores = Programacion a la defensiva
             Formatter flujoPersistencia = new Formatter(new File("saludo.txt"));
-            flujoPersistencia.format("%s\n", "Que mas?");
             flujoPersistencia.format("Suma: %.2f %.2f\n", 4.5, 9.1);
             flujoPersistencia.format("Resta: %.2f %.2f\n", -7.2, 3.5);
             flujoPersistencia.close();
         } catch (FileNotFoundException ex) {
             System.out.println(ex.getMessage());
+        }
+        
+        //FLUJO DE ENTRADA = Lectura: Del files al .java
+        try {
+            Scanner flujoLectura = new Scanner(new File("saludo.txt"));
+            flujoLectura.next();
+            double num1 = flujoLectura.nextDouble();
+            double num2 = flujoLectura.nextDouble();
+            System.out.println(num1 + ", " + num2);
+            
+            flujoLectura.next();
+            double num3 = flujoLectura.nextDouble();
+            double num4 = flujoLectura.nextDouble();
+            System.out.println(num3 + ", " + num4);
+           
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(FlujoFiles_BasadosCaracteres.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
