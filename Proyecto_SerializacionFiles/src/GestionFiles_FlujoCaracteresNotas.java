13a8d7bcccb30ff44f95cb8680ba8903e9e83b40
pdireneutpl <pdirene@utpl.edu.ec>
2025-05-30 12:29:12 -0500

    act

--- Proyecto_SerializacionFiles/src/GestionFiles_FlujoCaracteresNotas.java
+++ Proyecto_SerializacionFiles/src/GestionFiles_FlujoCaracteresNotas.java
@@ -1,14 +1,18 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Formatter;
 import java.util.Random;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 public class GestionFiles_FlujoCaracteresNotas {
     public static void main(String[] args) {
         String notasXavier[][] = new String[6][3];
+        String notasXavierEntrada[][] = new String[6][3];
         generarNotas(notasXavier);
         persistirNotasFiles(notasXavier);
+        lecturaNotasFiles(notasXavierEntrada);
     }
     
     public static void generarNotas(String notas[][]){
@@ -30,6 +34,18 @@ public class GestionFiles_FlujoCaracteresNotas {
                 objPersistenciaFile.format("%s;%s;%s;\n", notas[i][0], notas[i][1], notas[i][2]);
             }
             objPersistenciaFile.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(GestionFiles_FlujoCaracteresNotas.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public static void lecturaNotasFiles(String notasLeidas[][]){
+        try {
+            Scanner objLecturaFile = new Scanner(new File("notasEntrada.csv"));
+            while(objLecturaFile.hasNextLine()){
+                String materiaNotas[] = objLecturaFile.nextLine().split(";");
+                System.out.println(Arrays.toString(materiaNotas));
+            
+            }
         } catch (FileNotFoundException ex) {
             Logger.getLogger(GestionFiles_FlujoCaracteresNotas.class.getName()).log(Level.SEVERE, null, ex);
         }
