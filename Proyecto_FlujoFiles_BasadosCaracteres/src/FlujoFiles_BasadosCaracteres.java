f96e8cfe9c83090e34b141c5f2dded8900a399d3
pdirene <macbookpro@MacBookPro-pdirene.local>
2025-05-30 16:31:25 -0500

    act

--- Proyecto_FlujoFiles_BasadosCaracteres/src/FlujoFiles_BasadosCaracteres.java
+++ Proyecto_FlujoFiles_BasadosCaracteres/src/FlujoFiles_BasadosCaracteres.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+import java.util.Formatter;
+import java.io.File;
+import java.io.FileNotFoundException;
 public class FlujoFiles_BasadosCaracteres {
     public static void main(String[] args) {
+        /*//FLUJO SALIDA: del .java a la cmd
         System.out.println("Hola mundo");
+        //FLUJO ENTRADA: del cmd al .java
+        Scanner teclado = new Scanner(System.in); 
+        System.out.println(teclado.nextLine());  */
+        
+        //FLUJO SALIDA: del .java al file
+        try { //Gestion de errores = Programacion a la defensiva
+            Formatter flujoPersistencia = new Formatter(new File("saludo.txt"));
+            flujoPersistencia.format("%s", "Que mas?");
+            flujoPersistencia.format("Suma: %.2f %.2f", 4.5, 9.1);
+            flujoPersistencia.format("Resta: %.2f %.2f", -7.2, 3.5);
+            flujoPersistencia.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
