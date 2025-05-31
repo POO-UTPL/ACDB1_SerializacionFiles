1ef11d896e9989d824396c966e58f31188cb41b2
pdirene <macbookpro@MacBookPro-pdirene.local>
2025-05-31 10:02:35 -0500

    act

--- Proyecto_SerializacionFiles/src/GestionFiles_FlujoCaracteresNotas.java
+++ Proyecto_SerializacionFiles/src/GestionFiles_FlujoCaracteresNotas.java
@@ -1,14 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.text.NumberFormat;
-import java.text.ParseException;
 import java.util.Arrays;
 import java.util.Formatter;
-import java.util.Locale;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 public class GestionFiles_FlujoCaracteresNotas {
     public static void main(String[] args) {
         String notasXavier[][] = new String[6][3];
@@ -46,9 +44,14 @@ public class GestionFiles_FlujoCaracteresNotas {
     public static void lecturaNotasFiles(String notasLeidas[][]){
         try {
             Scanner objLecturaFile = new Scanner(new File("notasEntrada.csv"));
-            objLecturaFile.useLocale(Locale.US);
+            //objLecturaFile.useLocale(Locale.US);
+            int i = 0;
             while(objLecturaFile.hasNextLine()){
                 String materiaNotas[] = objLecturaFile.nextLine().split(";");
+                notasLeidas[i][0] = materiaNotas[0];
+                notasLeidas[i][1] = materiaNotas[1];
+                notasLeidas[i][2] = materiaNotas[2];
+                i++;
                 System.out.println(Arrays.toString(materiaNotas));
             }
             objLecturaFile.close();
@@ -57,13 +60,16 @@ public class GestionFiles_FlujoCaracteresNotas {
         }
     }
     public static void calcularResultados(String resultados[][], String notasXavierEntrada[][]){
-        //double nota1B1 = Double.parseDouble(notasXavierEntrada[0][1]);
+        double nota1B1 = 0, nota2B1 = 0, promedio = 0;
         for (int i = 0; i < notasXavierEntrada.length; i++) {
-            System.out.println(notasXavierEntrada[0][1]);
-                //double nota1B1 = Double.parseDouble(notasXavierEntrada[i][1]);
-                //System.out.println(nota1B1);
-                //double nota2B1 = fn.parse(notasXavierEntrada[i][2]).doubleValue();
-                //resultados[i][1] = ((nota1B1=nota2B1)/2 >= 6.5 ) ? "APRO" : "REPRO" ;
+            nota1B1 = Double.parseDouble(notasXavierEntrada[i][1]);
+            nota2B1 = Double.parseDouble(notasXavierEntrada[i][1]);
+            promedio = (nota1B1 + nota2B1) / 2;
+            resultados[i][0] = String.valueOf(promedio);
+            resultados[i][1] = (promedio >= 6.5 ) ? "APRO" : "REPRO" ;
         }
+    }
+    public static void escribirResultados(){
+        
     }
 }
