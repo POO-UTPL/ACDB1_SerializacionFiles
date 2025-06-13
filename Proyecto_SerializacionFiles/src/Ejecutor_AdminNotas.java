ac088ee9480510f554f3bb7e8b2324ad9a153536
pdirene <macbookpro@MacBookPro-pdirene.local>
2025-06-13 15:34:43 -0500

    Act

--- Proyecto_SerializacionFiles/src/Ejecutor_AdminNotas.java
+++ Proyecto_SerializacionFiles/src/Ejecutor_AdminNotas.java
@@ -1,3 +1,12 @@
 public class Ejecutor_AdminNotas {
     
 }
+class Estudiante{
+    public double nb1, nb2, promedio;
+    public String estado; //APROBADO, Reprobado
+    public Estudiante(double nb1, double nb2) {
+        this.nb1 = nb1; this.nb2 = nb2;  }
+
+    public String toString() {
+        return "Estudiante{" + "nb1=" + nb1 + ", nb2=" + nb2 + ", promedio=" + promedio + ", estado=" + estado + '}';}
+}
