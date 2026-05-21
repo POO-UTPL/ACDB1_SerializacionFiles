15c813e8257fed114807090475c5af754542662a
Pedro Daniel Irene Robalino <pdirene@utpl.edu.ec>
2026-05-21 09:43:48 -0500

    Act Proyecto 3

--- Proyecto_SistemaAprobAsigEst_V3/src/Controller/AsignaturaController.java
+++ Proyecto_SistemaAprobAsigEst_V3/src/Controller/AsignaturaController.java
@@ -1,4 +1,23 @@
 package Controller;
+
+import Model.*;
+import java.util.ArrayList;
+
 public class AsignaturaController {
+    public AsignaturaDAO asignaturaDAO;
+    public String nombreArchivoInNotas;
+    public ArrayList<Asignatura> asignaturas;
+
+    public AsignaturaController(String nombreArchivoInNotas) {
+        this.nombreArchivoInNotas = nombreArchivoInNotas;
+        this.asignaturaDAO = new AsignaturaDAO(this.nombreArchivoInNotas);
+    }
+    
+    public void calcularPromedio(){
+        asignaturas = asignaturaDAO.leerAsignaturas();
+        for (Asignatura asig : asignaturas){
+            asig.setPromedio( (asig.getB1() + asig.getB2()) / 2 );
+        }
+    }
     
 }
--- Proyecto_SistemaAprobAsigEst_V3/src/View/Ejecutor_SistemaAprobAsigEst.java
+++ Proyecto_SistemaAprobAsigEst_V3/src/View/Ejecutor_SistemaAprobAsigEst.java
@@ -1,6 +1,11 @@
 package View;
+
+import Controller.*;
+
 public class Ejecutor_SistemaAprobAsigEst {
     public static void main(String[] args) {
-        
+        AsignaturaController asignaturaController = new AsignaturaController("src/DB/NotasIn.csv");
+        asignaturaController.calcularPromedio();
+        System.out.println(asignaturaController.asignaturas);
     }
 }
