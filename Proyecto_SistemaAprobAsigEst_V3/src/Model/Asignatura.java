d97509221bf819241cbf232f2dd87fc11f04a1a2
Pedro Daniel Irene Robalino <pdirene@utpl.edu.ec>
2026-05-21 09:10:25 -0500

    Act Proyecto 3

--- Proyecto_SistemaAprobAsigEst_V3/src/Model/Asignatura.java
+++ Proyecto_SistemaAprobAsigEst_V3/src/Model/Asignatura.java
@@ -1,4 +1,69 @@
 package Model;
 public class Asignatura {
+    public String nombreAsig;
+    public double b1, b2, promedio, suple;
+    public boolean estado;
+
+    public Asignatura() {
+    }
+
+    public Asignatura(String nombreAsig, double b1, double b2) {
+        this.nombreAsig = nombreAsig;
+        this.b1 = b1;
+        this.b2 = b2;
+    }
+
+    public String getNombreAsig() {
+        return nombreAsig;
+    }
+
+    public void setNombreAsig(String nombreAsig) {
+        this.nombreAsig = nombreAsig;
+    }
+
+    public double getB1() {
+        return b1;
+    }
+
+    public void setB1(double b1) {
+        this.b1 = b1;
+    }
+
+    public double getB2() {
+        return b2;
+    }
+
+    public void setB2(double b2) {
+        this.b2 = b2;
+    }
+
+    public double getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(double promedio) {
+        this.promedio = promedio;
+    }
+
+    public double getSuple() {
+        return suple;
+    }
+
+    public void setSuple(double suple) {
+        this.suple = suple;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Asignatura{" + "nombreAsig=" + nombreAsig + ", b1=" + b1 + ", b2=" + b2 + ", promedio=" + promedio + ", suple=" + suple + ", estado=" + estado + '}';
+    }
     
 }
--- Proyecto_SistemaAprobAsigEst_V3/src/Model/AsignaturaDAO.java
+++ Proyecto_SistemaAprobAsigEst_V3/src/Model/AsignaturaDAO.java
@@ -1,4 +1,35 @@
 package Model;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AsignaturaDAO {
+    public Scanner fin;
+    public String nombreArchivoInNotas;
+    public ArrayList<Asignatura> asignaturas;
+
+    public AsignaturaDAO(String nombreArchivoInNotas) {
+        this.nombreArchivoInNotas = nombreArchivoInNotas;
+    }
     
+    public void crearFin(){
+        try {
+            fin = new Scanner(new File(this.nombreArchivoInNotas));
+        } catch (Exception e) {
+            System.out.println("ERROR AL CREAR EL FIN");
+        }
+    }
+    
+    public ArrayList<Asignatura> leerAsignaturas(){
+        crearFin();
+        String linea = fin.nextLine();
+        System.out.println(linea);
+        return asignaturas;
+    }
+    
+    public static void main(String[] args) {
+        AsignaturaDAO asigDAO = new AsignaturaDAO("src/DB/NotasIn.csv");
+        asigDAO.leerAsignaturas();
+    }
 }
